f8a664a1ca059da30139d6d0ab7cd933919b70fa
williamd5 <willduncannn@gmail.com>
2022-10-31 10:48:53 +0200

    fix paths

--- gen/index.ts
+++ gen/index.ts
@@ -1,15 +1,15 @@
 import Schema from "./Schema";
-import path from "node:path";
 import fs from "node:fs/promises";
 import {Config} from "./Config";
+import path from "node:path";
 import Mustache from "mustache";
 
 // load `/schema.json`
-const schema: Schema = JSON.parse(await fs.readFile(path.join("..", "schema.json"), "utf8"));
+const schema: Schema = JSON.parse(await fs.readFile("schema.json", "utf8"));
 
 // create folder `/src` if it doesn't exist
 try {
-    await fs.mkdir(path.join("..", "src"));
+    await fs.mkdir("src");
 }
 catch (e) {
     const err = e as NodeJS.ErrnoException;
@@ -17,10 +17,10 @@ catch (e) {
 }
 
 // load `/gen/config.json`
-const config: Config = JSON.parse(await fs.readFile("config.json", "utf8"));
+const config: Config = JSON.parse(await fs.readFile(path.join("gen", "config.json"), "utf8"));
 
 // load render main class from `/gen/templates/main.mustache`
-const mainTemplate = await fs.readFile(path.join("templates", "main.mustache"), "utf8");
+const mainTemplate = await fs.readFile(path.join("gen", "templates", "main.mustache"), "utf8");
 const mainRender = Mustache.render(mainTemplate, {schema, config});
 // write file to `/src/{{config.name}}.ts`
-await fs.writeFile(path.join("..", "src", `${config.name}.ts`), mainRender);
+await fs.writeFile(path.join("src", `${config.name}.ts`), mainRender);
