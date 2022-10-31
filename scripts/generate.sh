a71e1db6bb2055a69efc7337aa52b65b9b640e34
williamd5 <willduncannn@gmail.com>
2022-10-31 17:43:16 +0200

    add scripts to build and generate

---
+++ scripts/generate.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Name of main class
+MAIN_NAME=$(node scripts/getNameOfMain)
+
+# delete existing src/MAIN_NAME.ts if it exists
+if [ -f "src/$MAIN_NAME.ts" ]; then
+  rm "src/$MAIN_NAME.ts"
+fi
+
+# build project
+npm run build
+
+# run generator
+node gen
+
+# build project again (to include generated files)
+npm run build
