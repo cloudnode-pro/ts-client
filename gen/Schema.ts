a09b71321b1496e00e49625ece25d48b8c4fdbac
williamd5 <willduncannn@gmail.com>
2022-10-31 10:37:14 +0200

    modify models schema

--- gen/Schema.ts
+++ gen/Schema.ts
@@ -8,12 +8,16 @@ namespace Schema {
 
     export namespace Model {
         export interface Field {
+            name: string;
             description: string;
             type: Schema.Type;
         }
     }
 
-    export type Model = Record<string, Model.Field>;
+    export interface Model {
+        description: string;
+        fields: Schema.Model.Field[];
+    }
 
     export namespace Operation {
         export interface Parameter {
@@ -30,7 +34,7 @@ namespace Schema {
 
         export interface Namespace {
             type: "namespace";
-            operations: Record<string, Operation>;
+            operations: Record<string, Schema.Operation>;
         }
     }
 
@@ -40,11 +44,11 @@ namespace Schema {
         method: "GET" | "HEAD" | "POST" | "PUT" | "PATCH" | "DELETE";
         path: string;
         parameters: {
-            path?: Record<string, Operation.Parameter>;
-            query?: Record<string, Operation.Parameter>;
-            body?: Record<string, Operation.Parameter>;
+            path?: Record<string, Schema.Operation.Parameter>;
+            query?: Record<string, Schema.Operation.Parameter>;
+            body?: Record<string, Schema.Operation.Parameter>;
         }
-        returns: Operation.Response[];
+        returns: Schema.Operation.Response[];
         /**
          * Token scope required for this operation.
          *  - `undefined` - No token required.
