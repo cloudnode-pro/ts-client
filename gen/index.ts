31f6ce12279f0e2a1b38f0893d4ca7a4683847c9
williamd5 <willduncannn@gmail.com>
2022-11-01 16:14:36 +0200

    load package.json

--- gen/index.ts
+++ gen/index.ts
@@ -3,9 +3,13 @@ import fs from "node:fs/promises";
 import {Config} from "./Config";
 import path from "node:path";
 import Mustache from "mustache";
+import Package from "./Package";
 
 // load `/schema.json`
 const schema: Schema = JSON.parse(await fs.readFile("schema.json", "utf8"));
+
+// load `/package.json`
+const pkg: Package = JSON.parse(await fs.readFile("package.json", "utf8"));
 
 // create folder `/src` if it doesn't exist
 try {
