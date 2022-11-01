a2fee870caa4d2eafceefff722f2616143f14887
williamd5 <willduncannn@gmail.com>
2022-11-01 16:44:34 +0200

    generate readme only

---
+++ gen/readme.ts
@@ -0,0 +1,21 @@
+import {generateDocSchema, generateMarkdownDocs, generateReadme} from "./docs.js";
+import {Config} from "./Config";
+import fs from "node:fs/promises";
+import path from "node:path";
+import Schema from "./Schema";
+import Package from "./Package";
+
+// load `/schema.json`
+const schema: Schema = JSON.parse(await fs.readFile("schema.json", "utf8"));
+
+// load `/package.json`
+const pkg: Package = JSON.parse(await fs.readFile("package.json", "utf8"));
+
+// load `/gen/config.json`
+const config: Config = JSON.parse(await fs.readFile(path.join("gen", "config.json"), "utf8"));
+
+// generate doc schema
+const docSchema = await generateDocSchema(schema, config, pkg);
+// generate readme
+const docMD = generateMarkdownDocs(docSchema, true);
+await generateReadme(docMD, config, pkg);
