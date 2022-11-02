bea569b165b4ff84a0b6f8d03b7234436acf6dfa
williamd5 <willduncannn@gmail.com>
2022-11-02 09:14:17 +0200

    move building generated code to gen/index

--- gen/index.ts
+++ gen/index.ts
@@ -6,6 +6,8 @@ import source from "./source.js";
 import {generateDocSchema, generateMarkdownDocs, generateReadme} from "./docs.js";
 import Package from "./Package";
 import {addExtraReturns} from "./util.js";
+import * as child_process from "child_process";
+import {promisify} from "util";
 
 // load `/schema.json`
 const schema: Schema = addExtraReturns(JSON.parse(await fs.readFile("schema.json", "utf8")));
@@ -28,6 +30,8 @@ const config: Config = JSON.parse(await fs.readFile(path.join("gen", "config.jso
 // generate source code
 await source(schema, config, pkg);
 
+// build
+await promisify(child_process.exec)("npm run build");
 // generate doc schema
 const docSchema = await generateDocSchema(schema, config, pkg);
 // generate readme
