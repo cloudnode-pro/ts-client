2cd8a45019e2e060bcc034295741b6b9faca6819
William Duncan <willduncannn@gmail.com>
2023-05-07 15:25:33 +0300

    2.1.0

--- src/Cloudnode.ts
+++ src/Cloudnode.ts
@@ -42,7 +42,7 @@ class Cloudnode {
     /**
      * Client user agent
      */
-    readonly #userAgent = `cloudnode/2.0.0`;
+    readonly #userAgent = `cloudnode/2.1.0`;
 
     /**
      * Construct a new Cloudnode API client
